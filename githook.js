0f0094ce0e5b0e1b30b7d95212d5afd77453da48
root <root@146.185.138.227.russiapolitics.ru>
2014-10-22 09:47:31 +0000

    test githubhook

---
+++ githook.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var githubhook = require('githubhook');
+
+var github = githubhook({/* options */});
+
+github.listen();
+
+// github.on('*', function (event, repo, ref, data) {
+// });
+
+// github.on('push', function (repo, ref, data) {
+// });
+
+// github.on('event:reponame', function (ref, data) {
+// });
+
+github.on('push:jsboom-bem:refs/heads/master', function (data) {
+	console.log('data',data);
+});
+
+// github.on('reponame', function (event, ref, data) {
+// });
+
+// github.on('reponame:ref', function (event, data) {
+// });
