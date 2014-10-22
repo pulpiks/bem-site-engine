08b27d541cf00563552c8ff126bb05df08a5dcc3
root <root@146.185.138.227.russiapolitics.ru>
2014-10-22 14:37:10 +0000

    add kseniua to gogle

--- model/index.js
+++ model/index.js
@@ -9,36 +9,15 @@ var fs = require('fs');
 module.exports = {
     get: function() {
         return [
-            getMain(),
+            // getMain(),
             getCustom()
-            // getDocs(),
+            // getDocs()
             // getLibraries(),
             // getAuthors(),
             // getTags()
         ];
     }
 };
-
-var getMain = function() {
-    
-    return {
-        title: 'Статьи',
-        route: {
-            name: 'index',
-            pattern: '/'
-        },
-        source: {
-            ru: {
-                title: 'Bem-site-engine',
-                createDate: '12-07-2014',
-                authors: ['kuznetsov-andrey'],
-                tags: ['readme'],
-                content: 'https://github.com/bem/bem-site-engine/blob/dev/README.md'
-            }
-        }
-    };
-};
-
 
 var getCustom = function() {
     var dirmd = './md';
@@ -93,69 +72,69 @@ var getCustom = function() {
 };
 
 
-var getDocs = function() {
-    return {
-        title: 'Документация',
-        route: {
-            name: 'documentation',
-            pattern: '/documentation(/<id>)(/)'
-        },
-        items: [
-            {
-                title: 'Создание модели',
-                route: 'model',
-                source: {
-                    ru: {
-                        title: 'Создание модели',
-                        createDate: '12-07-2014',
-                        authors: ['kuznetsov-andrey'],
-                        tags: ['documentation', 'model'],
-                        content: 'https://github.com/bem/bem-site-engine/blob/dev/docs/model.ru.md'
-                    }
-                }
-            },
-            {
-                title: 'Конфигурация',
-                route: 'config',
-                source: {
-                    ru: {
-                        title: 'Руководство по конфигурированию приложения',
-                        createDate: '12-07-2014',
-                        authors: ['kuznetsov-andrey'],
-                        tags: ['documentation', 'config'],
-                        content: 'https://github.com/bem/bem-site-engine/blob/dev/docs/config.ru.md'
-                    }
-                }
-            },
-            {
-                title: 'Описание middleware модулей',
-                route: 'middleware',
-                source: {
-                    ru: {
-                        title: 'Описание middleware модулей',
-                        createDate: '12-07-2014',
-                        authors: ['kuznetsov-andrey'],
-                        tags: ['documentation', 'middleware'],
-                        content: 'https://github.com/bem/bem-site-engine/blob/dev/docs/middleware.ru.md'
-                    }
-                }
-            },
-            {
-                title: 'Процесс сборки данных',
-                route: 'compile',
-                source: {
-                    ru: {
-                        title: 'Процесс сборки данных',
-                        createDate: '12-07-2014',
-                        authors: ['kuznetsov-andrey'],
-                        tags: ['documentation', 'data-compile'],
-                        content: 'https://github.com/bem/bem-site-engine/blob/dev/docs/data_compiling.ru.md'
-                    }
-                }
-            }
-        ]
-    };
-};
+// var getDocs = function() {
+//     return {
+//         title: 'Документация',
+//         route: {
+//             name: 'documentation',
+//             pattern: '/documentation(/<id>)(/)'
+//         },
+//         items: [
+//             {
+//                 title: 'Создание модели',
+//                 route: 'model',
+//                 source: {
+//                     ru: {
+//                         title: 'Создание модели',
+//                         createDate: '12-07-2014',
+//                         authors: ['kuznetsov-andrey'],
+//                         tags: ['documentation', 'model'],
+//                         content: 'https://github.com/bem/bem-site-engine/blob/dev/docs/model.ru.md'
+//                     }
+//                 }
+//             },
+//             {
+//                 title: 'Конфигурация',
+//                 route: 'config',
+//                 source: {
+//                     ru: {
+//                         title: 'Руководство по конфигурированию приложения',
+//                         createDate: '12-07-2014',
+//                         authors: ['kuznetsov-andrey'],
+//                         tags: ['documentation', 'config'],
+//                         content: 'https://github.com/bem/bem-site-engine/blob/dev/docs/config.ru.md'
+//                     }
+//                 }
+//             },
+//             {
+//                 title: 'Описание middleware модулей',
+//                 route: 'middleware',
+//                 source: {
+//                     ru: {
+//                         title: 'Описание middleware модулей',
+//                         createDate: '12-07-2014',
+//                         authors: ['kuznetsov-andrey'],
+//                         tags: ['documentation', 'middleware'],
+//                         content: 'https://github.com/bem/bem-site-engine/blob/dev/docs/middleware.ru.md'
+//                     }
+//                 }
+//             },
+//             {
+//                 title: 'Процесс сборки данных',
+//                 route: 'compile',
+//                 source: {
+//                     ru: {
+//                         title: 'Процесс сборки данных',
+//                         createDate: '12-07-2014',
+//                         authors: ['kuznetsov-andrey'],
+//                         tags: ['documentation', 'data-compile'],
+//                         content: 'https://github.com/bem/bem-site-engine/blob/dev/docs/data_compiling.ru.md'
+//                     }
+//                 }
+//             }
+//         ]
+//     };
+// };
 
 var getLibraries = function() {
     return {
