a0ee1e8efe017f2beb4caa6db55219900642f1ed
root <root@146.185.138.227.russiapolitics.ru>
2014-10-22 12:04:35 +0000

    t

--- githook.js
+++ githook.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var createHandler = require('github-webhook-handler');
 var handler = createHandler({ path: '/webhook', secret: 'ksenia' });
-
 http.createServer(function (req, res) {
 	console.log('ffff');
   handler(req, res, function (err) {
@@ -9,11 +8,9 @@ http.createServer(function (req, res) {
     res.end('no such location')
   })
 }).listen(443)
-
 handler.on('error', function (err) {
   console.err('Error:', err.message)
 })
-
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
